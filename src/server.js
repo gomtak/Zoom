a6fd63f924daf042818d4a9c76ac4dcf1d218a03
tak6831 <tak6831@playnomm.com>
2022-07-11 14:56:56 +0900

    Feat : Add ws import
    express에서 ws를 지원하지 않기 때문에 둘다 받을 수 있는 서버 세팅 추가

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+import http from "http";
+import WebSocket from "ws";
 import express, { application } from "express";
+
 
 const app = express();
 
@@ -7,5 +10,12 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+// const handleListen = () => console.log('Listening on ws://localhost:3000');
+// app.listen(3000, handleListen); express 는 웹소캣을 지원하지 않기 때문에 아래와 같이 변경해서 같이 쓸수 있게 만든다.
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+server.listen(3000, handleListen);

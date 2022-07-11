9dbf3627c88a332685b8aa4816dd5504b6c62b94
tak6831 <tak6831@playnomm.com>
2022-07-11 16:28:26 +0900

    Feat : Add message method each other server, browser

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,18 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
-console.log(socket);
+
+socket.addEventListener("open",() => {
+    console.log("Connected to Browser ✅");
+})
+
+socket.addEventListener("message", (message) => {
+    // console.log("Just got this: ", message.data, " from the Server");
+    console.log("New message : ", message.data);
+})
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server ❌");
+})
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 2000);

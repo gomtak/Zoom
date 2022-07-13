6d711c76b6f9a919b26df39788b58e28500ca904
tak6831 <tak6831@playnomm.com>
2022-07-13 18:03:26 +0900

    feat: Add socket.io callback fun

--- src/server.js
+++ src/server.js
@@ -20,8 +20,12 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-    socket.on("enter_room", (msg) => console.log(msg))
-    
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg)
+        setTimeout(() =>{
+            done();
+        }, 1000);
+    })
 })
 
 

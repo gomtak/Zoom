6e9ac6f5040e74ee4987d3ba9df752fabbad0768
tak6831 <tak6831@playnomm.com>
2022-07-14 10:41:20 +0900

    feat: Add room join

--- src/server.js
+++ src/server.js
@@ -20,11 +20,12 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-    socket.on("enter_room", (msg, done) => {
-        console.log(msg)
-        setTimeout(() =>{
-            done("hello from the backend");
-        }, 1000);
+    socket.onAny((event) => {
+        console.log(`Socket Event:${event}`)
+    })
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
     })
 })
 

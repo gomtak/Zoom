f83c367b29d56e46d10698fed36f028d18cc8ed3
gomtak <tak6831@gmail.com>
2022-07-16 23:21:28 +0900

    Add room and join the room

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,10 +5,15 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const cameraSelect = document.getElementById("cameras");
 
+
+const call = document.getElementById("call");
+
+call.hidden = true;
+
 let myStream;
 let muted = false;
 let cameraOff = false;
-
+let roomName;
 async function getCameras(){
     try {
         const devices = await navigator.mediaDevices.enumerateDevices()
@@ -55,7 +60,7 @@ async function getMedia(deviceId){
     }
 }
 
-getMedia();
+
 
 function handleMuteClick(){
     myStream
@@ -91,3 +96,29 @@ muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 cameraSelect.addEventListener("input",handleCameraChange);
 
+// Welcome Form (join a room)
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+function startMedia(){
+    welcome.hidden = true;
+    call.hidden = false;
+    getMedia();
+}
+
+function handleWelcomeSubmit(event) {
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    socket.emit("join_room", input.value, startMedia);
+    roomName = input.value;
+    input.value = "";
+}
+
+welcomeForm.addEventListener("submit",handleWelcomeSubmit);
+
+// Socket code
+
+socket.on("welcome", () => {
+    console.log("some on joined!");
+})
